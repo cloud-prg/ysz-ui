2cf6205f4f286b36f97ad4587f836ba49650ab31
陈开邦 <912453237@qq.com>
2022-08-20 17:17:36 +0800

    feature: 修改打包方式

--- src/utils/handleBuild.js
+++ src/utils/handleBuild.js
@@ -1,10 +1,19 @@
 const copyDir = require('./copyDir'); // 调用拷贝目录方法
 const path = require("path");
 
+
+// 拷贝iconfont到
 const sourcePath = path.resolve(__dirname, "../assets/iconfont/"); // 源目录
 const targetPath = path.resolve(__dirname, "../../dist/"); // 要移向的目录
 
-
 copyDir(sourcePath, targetPath, err => {
+    console.log("err", err);
+})
+
+// 打包后的assets放到根目录
+const assetsSourcePath = path.resolve(__dirname, "../../dist/assets"); // 源目录
+const assetsTargetPath = path.resolve(__dirname, "../../assets"); // 要移向的目录
+
+copyDir(assetsSourcePath, assetsTargetPath, err => {
     console.log("err", err);
 })
