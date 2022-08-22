0909f055e8b03a20339d26bc79d65d1c14882892
陈开邦 <912453237@qq.com>
2022-08-22 21:24:17 +0800

    sh update

--- deploy.sh
+++ deploy.sh
@@ -20,5 +20,5 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:YunShangZhou/ysz-ui.git pages:gh-pages
+git push -f git@github.com:YunShangZhou/ysz-ui.git master:gh-pages
 cd -
